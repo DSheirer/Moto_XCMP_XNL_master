168f46e7527406824f5d373a1485829101d095e0
xjapanseikima <magican213@gmail.com>
2019-05-21 18:33:28 -0700

    ...

---
+++ src/priv/chunyu/moto/TEA/TEA.java
@@ -0,0 +1,9 @@
+package priv.chunyu.moto.TEA;
+
+public class TEA {
+	public TEA( String keydata) {
+        int[] key = new int[]{0x11223344, 0x55667788, 0x99AABBCC, 0xDDEEFF00};
+		System.out.print(keydata);
+	}
+	
+}
