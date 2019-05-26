1c5186ac72753278bb0a2138d314007477e48b6e
xjapanseikima <magican213@gmail.com>
2019-05-25 19:17:27 -0700

    need encrypted key

--- src/priv/chunyu/moto/TEA/TEA.java
+++ src/priv/chunyu/moto/TEA/TEA.java
@@ -4,7 +4,7 @@ public class TEA {
 		int[] data = new int[2];
 		String x;
 	public TEA(String[] keydata) {
-		int[] key = new int[] {0x152C7E9D,0x38BE41C7,0x71E96CA4,0x6CAC1AFC };
+		int[] key = new int[] {Enter by urself };
 		data[0] = (int) Long.parseLong(keydata[0], 16);
 		data[1] = (int) Long.parseLong(keydata[1], 16);
 		int[] encrypt= encrypt(data, key);
@@ -17,7 +17,7 @@ public class TEA {
 		int i = block[0];
 		int j = block[1];
 		int sum = 0;
-		int delta = 0x9E3779B9;
+		int delta = Enter by urself ;
 		for (int k = 0; k < 32; ++k) {
 			sum += delta;
 			i += (j << 4 & 0xfffffff0) + key[0] ^ j + sum ^ (j >> 5 & 0x7ffffff) + key[1];
@@ -48,7 +48,7 @@ public class TEA {
 	private static int[] decrypt(int[] block, int[] key) {
 		int i = block[0];
 		int j = block[1];
-		int delta = 0x9E3779B9;
+		int delta = Enter by urself ;
 		int sum = delta * 32; // sum=468ACF00
 		for (int k = 0; k < 32; ++k) {
 			j -= (i << 4 & 0xfffffff0) + key[2] ^ i + sum ^ (i >> 5 & 0x7ffffff) + key[3];
