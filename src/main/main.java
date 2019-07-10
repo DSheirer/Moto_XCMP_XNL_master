bff474dc165ac10099ffaa4d0328a9625acae6f2
xjapanseikima <magican213@gmail.com>
2019-07-10 16:49:30 -0700

    main

--- src/main/main.java
+++ src/main/main.java
@@ -1,11 +1,14 @@
 package main;
 
 import java.io.IOException;
+
+import GUI.StartInterface;
 import priv.chunyu.moto.xnl.XNLsocket;
 
 public class main {
 	public static void main(String args[]) throws IOException, InterruptedException {
 		XNLsocket Xnl_Connection = new XNLsocket();
 		Xnl_Connection.run();
+		new StartInterface ();
 	}
 }
