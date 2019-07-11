c4dd1649c96423df44759f6f5dfb7857832ed8ae
xjapanseikima <magican213@gmail.com>
2019-07-10 17:00:36 -0700

    XNL

--- src/priv/chunyu/moto/xnl/XNL.java
+++ src/priv/chunyu/moto/xnl/XNL.java
@@ -3,6 +3,8 @@ package priv.chunyu.moto.xnl;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
@@ -10,9 +12,15 @@ public class XNL extends Thread {
 	public static Socket master;
 	public static DataOutputStream output;
 	public static DataInputStream input;
-	public static void setConnection() throws UnknownHostException, IOException {
+	public static void setConnection() throws  IOException {
+		checkConnection();
 		master = new Socket("192.168.10.1", 8002);// Pc set as master ,and setup connection;// Pc
 		output = new DataOutputStream(master.getOutputStream());
 		input = new DataInputStream(master.getInputStream());// data input
+		
+	}
+	private static void checkConnection() {
+		//master.connect(new InetSocketAddress("192.168.10.1", 8002), 1000);
+		
 	}
 }
